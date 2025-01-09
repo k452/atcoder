2a3afe0d89532c58b442d1a3aa62162783d8808d
k452 <55301839+k452@users.noreply.github.com>
2025-01-10 02:32:43 +0900

    不要ディレクトリ除去

--- typical90/001/target/debug/_001.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/code/hobby/atcoder/typical90/001/target/debug/_001: /home/ubuntu/code/hobby/atcoder/typical90/001/src/main.rs
--- typical90/001/target/debug/deps/_001-e86c3bc48e61f8d3.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/code/hobby/atcoder/typical90/001/target/debug/deps/lib_001-e86c3bc48e61f8d3.rmeta: src/main.rs
-
-/home/ubuntu/code/hobby/atcoder/typical90/001/target/debug/deps/_001-e86c3bc48e61f8d3.d: src/main.rs
-
-src/main.rs:
